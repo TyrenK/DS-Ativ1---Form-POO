628662ccad18cb16fe48aa14a236004673c978fc
TyrenK <bizinottoluca@gmail.com>
2025-03-10 09:34:42 -0300

    primeiro commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main (String[] args){
+        System.out.println("Bem-vindo ao Sistema de Cadastro de Pessoas! \nEscolha uma operação: \n");
+
+        while ( != 5) {
+            System.out.println("1 - Cadastrar uma nova pessoa \n2 - Editar dados de uma pessoa cadastrada \n3 - Apresentar dados cadastrados \n4 - Excluir dados cadastrados \n5 - Sair\n");
+        }
+    }
+}
---
+++ Pessoa.java
@@ -0,0 +1,27 @@
+public class Pessoa {
+    private String nome, email;
+    private int idade, op;
+
+    public Pessoa(String nome, String email, int idade){
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+ 
+    public String getNome(){
+        return nome;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+    
+    public int getIdade(){
+        return idade;
+    }
+
+
+
+
+
+}
